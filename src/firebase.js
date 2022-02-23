9341f110e03cb436620da5dc502e252e0110441e
joaofranciscosousa <89461082+joaofranciscosousa@users.noreply.github.com>
2022-02-23 00:45:04 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDAfVJwrXI6shjCNCwPSyZ5dWJ6AzzRw-4",
-    authDomain: "instagram-83455.firebaseapp.com",
-    projectId: "instagram-83455",
-    storageBucket: "instagram-83455.appspot.com",
-    messagingSenderId: "773003383780",
-    appId: "1:773003383780:web:9a651f84ca43a3d389adf1"
+    apiKey: "****************************",
+    authDomain: "****************************",
+    projectId: "****************************",
+    storageBucket: "****************************",
+    messagingSenderId: "****************************",
+    appId: "****************************"
   };
 
 const app = initializeApp(firebaseConfig);
